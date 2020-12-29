b256402697ec96ca12678597f397f3d83ca96680
MohdFaisalAliKhan <faisalakhan98@gmail.com>
2020-12-29 20:15:24 +0530

    third commit

--- wp-content/themes/xyz-template/template-parts/content-none.php
+++ wp-content/themes/xyz-template/template-parts/content-none.php
@@ -10,9 +10,9 @@
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'xyz-template' ); ?></h1>
-	</header><!-- .page-header -->
+	
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found ! Sorry.', 'xyz-template' ); ?></h1>
+	
 
 	<div class="page-content">
 		<?php
@@ -36,14 +36,14 @@
 
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'xyz-template' ); ?></p>
 			<?php
-			get_search_form();
+			// get_search_form();
 
 		else :
 			?>
 
 			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'xyz-template' ); ?></p>
 			<?php
-			get_search_form();
+			// get_search_form();
 
 		endif;
 		?>
